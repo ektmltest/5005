694e3c0aeb63e147ed070c897fbe6f844d661652
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-05-17 16:37:35 +0300

    Repo design pattern

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Interfaces\ProjectRepositoryInterface;
+use App\Interfaces\UserRepositoryInterface;
+use App\Repositories\ProjectRepository;
+use App\Repositories\UserRepository;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
@@ -11,7 +15,8 @@ class RepositoryServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        // $this->app->bind(interface, class)
+        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+        $this->app->bind(ProjectRepositoryInterface::class, ProjectRepository::class);
     }
 
     /**
---
+++ app/Repositories/ProjectRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Repositories;
+use App\Interfaces\ProjectRepositoryInterface;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectRepository implements ProjectRepositoryInterface {
+}
