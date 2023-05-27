dc9bbc5a3fdcbbb62493f6e9140d0db270f237ba
Omar ElGohary <omarelgohary82@gmail.com>
2023-05-27 16:50:12 +0200

    new

--- app/Http/Livewire/Website/Like.php
+++ app/Http/Livewire/Website/Like.php
@@ -3,47 +3,47 @@ namespace App\Http\Livewire\Website;
 use App\Models\ReadyProject;
 use Livewire\Component;
 
-class Like extends Component
-{
-    public function addorremovelikes($id)
-    {
-        $type = request("type");
-        $flag = false;
-        $action="append";
-        if($type=="product"){
-            $product = ReadyProject::findOrFail($id);
-        if($product->likes->where("user_id",auth()->user()->id)->count()==0){
-            $product->likes()->create([
-                "user_id" => auth()->user()->id,
-                "type"=> $type,
-            ]);
-            $action="add";
-        }else{
-            $product->likes()->where("user_id",auth()->user()->id)->delete();
-            $action="delete";
-            }
+// class Like extends Component
+// {
+    // public function addorremovelikes($id)
+    // {
+    //     $type = request("type");
+    //     $flag = false;
+    //     $action="append";
+    //     if($type=="product"){
+    //         $product = ReadyProject::findOrFail($id);
+    //     if($product->likes->where("user_id",auth()->user()->id)->count()==0){
+    //         $product->likes()->create([
+    //             "user_id" => auth()->user()->id,
+    //             "type"=> $type,
+    //         ]);
+    //         $action="add";
+    //     }else{
+    //         $product->likes()->where("user_id",auth()->user()->id)->delete();
+    //         $action="delete";
+    //         }
 
-        $flag=true;
+    //     $flag=true;
 
-        }elseif($type=="photo"){
-            $photo = Photo::findOrFail($id);
-            if($photo->likes->where("user_id",auth()->user()->id)->count()==0){
-                $photo->likes()->create([
-                    "user_id" => auth()->user()->id,
-                    "type"=> $type,
-                ]);
-                $action="add";
-            }else{
-                $photo->likes()->where("user_id",auth()->user()->id)->delete();
-                $action="delete";
-                }
-            $flag=true;
-            }
-        return json_encode(["status"=>$flag,"action"=>$action],true);
-    }
+//         }elseif($type=="photo"){
+//             $photo = Photo::findOrFail($id);
+//             if($photo->likes->where("user_id",auth()->user()->id)->count()==0){
+//                 $photo->likes()->create([
+//                     "user_id" => auth()->user()->id,
+//                     "type"=> $type,
+//                 ]);
+//                 $action="add";
+//             }else{
+//                 $photo->likes()->where("user_id",auth()->user()->id)->delete();
+//                 $action="delete";
+//                 }
+//             $flag=true;
+//             }
+//         return json_encode(["status"=>$flag,"action"=>$action],true);
+//     }
 
-    public function render()
-    {
-        return view('livewire.website.like');
-    }
-}
+//     public function render()
+//     {
+//         return view('livewire.website.like');
+//     }
+// }
