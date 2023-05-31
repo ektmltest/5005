29af223203e65bdb924ca7cd3aa65d61b8f3a6fd
Omar ElGohary <omarelgohary82@gmail.com>
2023-05-31 12:19:13 +0200

    dashboard locale

--- lang/en/dashboard_trans.php
+++ lang/en/dashboard_trans.php
@@ -4,6 +4,25 @@ return [
     'Dashboard' => 'Dashboard - Ektml',
     'dashboard' => 'Dashboard',
 
+    'MAIN OPTIONS' => 'MAIN OPTIONS',
+    'Home' => 'Home',
+
+    'PROJECTS SYSTEM' => 'PROJECTS SYSTEM',
+    'Projects management' => 'Projects management',
+    'Projects sections' => 'Projects sections',
+    'Projects categories' => 'Projects categories',
+
+    'CATALOG MANAGMENT' => 'CATALOG MANAGMENT',
+    'Manage Projects' => 'Manage Projects',
+    'Add Project' => 'Add Project',
+    'Store Catogries' => 'Store Catogries',
+    'Addons' => 'Addons',
+
+
+
+
+
+
 
 
 
