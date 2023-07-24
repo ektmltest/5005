ca28292a08b1cc7437b2cc83857c05c1af1f801a
kareemshaaban221 <kareemshaaban221@gmail.com>
2023-07-24 11:30:11 +0300

    portifolio error

--- resources/views/livewire/website/like.blade.php
+++ resources/views/livewire/website/like.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div id="portfolio-grid" class="row no-gutter">
+    <div class="row no-gutter">
         @foreach ($ready_projects as $project)
         <div class="item prj-{{$project->department->id}} col-md-4 col-12 mb-4" style="max-height: 400px">
             <div class="post-item">
@@ -18,7 +18,7 @@
                         <li>{{ $project->created_at->diffForHumans() }}</li>
                         <li>{{ $project->likes->count() }} {{ __('main_trans.Likes')}}</li>
                     </ul>
-                    <p>{{ $project->description }}</p>
+                    <p>{{ Str::limit($project->description, 100, '...') }}</p>
                     <div class="footer-post">
                         <div class="tags">
                             <a href="{{ route('project', $project->id) }}">
